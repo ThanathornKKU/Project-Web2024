ccbbaa4704d902c9fcc5ce88245f0156d5152777
ThanathornKKU <thanathorn.c@kkumail.com>
2025-03-02 03:03:00 +0700

    Update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  distDir: 'dist',
   images: {
     domains: ["lh3.googleusercontent.com", "www.shutterstock.com"], // ✅ เพิ่ม Shutterstock
   },
