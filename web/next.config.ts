9a5974ee4ba568478d85ad700dc2c460998a5ba8
ThanathornKKU <thanathorn.c@kkumail.com>
2025-03-02 03:39:42 +0700

    update next.config and package.json

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "standalone", // ✅ ใช้ Dynamic Routing แทน Static Export
   distDir: 'dist',
   images: {
     domains: ["lh3.googleusercontent.com", "www.shutterstock.com"], // ✅ เพิ่ม Shutterstock
